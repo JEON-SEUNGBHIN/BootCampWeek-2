2cac32ba63fcf9218d3edd1f73cbe8960257b0e8
Sol Park <solpark16@gmail.com>
2024-05-02 11:30:54 +0900

    make heart button event

---
+++ js/heart.js
@@ -0,0 +1,29 @@
+// import { movieListAPI, handleSearch } from "./movie.js";
+const HEART_LS = "hearts";
+let hearts = [];
+
+const heartBtn = document.querySelector(".heartBtn");
+function clickHeart(event) {
+  // 중복값 방지 조건문
+  if (!hearts.includes(event.target.id.toString())) {
+    // 찜하지 않은 상태에서 클릭 시 등록 이벤트
+    hearts.push(event.target.id);
+    localStorage.setItem(HEART_LS, JSON.stringify(hearts));
+    alert("찜한 목록에 저장되었습니다.");
+    heartBtn.classList.add("clicked");
+  } else {
+    // 찜한 상태에서 다시 클릭 시 취소 이벤트
+    const thisIdx = hearts.indexOf(event.target.id);
+    hearts.splice(thisIdx, 1);
+    localStorage.setItem(HEART_LS, JSON.stringify(hearts));
+    alert("찜한 목록에서 삭제되었습니다.");
+    heartBtn.classList.remove("clicked");
+  }
+  console.log(hearts);
+}
+heartBtn.addEventListener("click", clickHeart);
+
+// const createHeart = async (movies) => {
+//   console.log(movies);
+// };
+// createHeart();
--- js/movie.js
+++ js/movie.js
@@ -1,43 +1,55 @@
-const ApiKey = '21ccf5793f9e51cfba0198fa23b3d541';
-const ApiToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMWNjZjU3OTNmOWU1MWNmYmEwMTk4ZmEyM2IzZDU0MSIsInN1YiI6IjY2MmEwZDFkYmYzMWYyMDA5YWUzMzAzYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sNlesGPpMfB6Nt3ZqEFMSIwcE88KWjPts2Waw_I2qp8';
+const ApiKey = "21ccf5793f9e51cfba0198fa23b3d541";
+const ApiToken =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMWNjZjU3OTNmOWU1MWNmYmEwMTk4ZmEyM2IzZDU0MSIsInN1YiI6IjY2MmEwZDFkYmYzMWYyMDA5YWUzMzAzYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sNlesGPpMfB6Nt3ZqEFMSIwcE88KWjPts2Waw_I2qp8";
 
 const $movieList = document.querySelector("#movie-list");
 let movies;
 
 export const handleSearch = (e) => {
-    e.preventDefault();
-    let movieSearch = movies.filter((movie) => movie.title.toLowerCase().includes(e.target[0].value.toLowerCase()));
-    createMovieList(movieSearch);
-}
+  e.preventDefault();
+  let movieSearch = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(e.target[0].value.toLowerCase())
+  );
+  createMovieList(movieSearch);
+};
 
-export const movieListAPI = async (url = "/3/movie/popular?language=en-US&page=1") => {
-    const api = await ApiFetch(url);
-    movies = api.results;
-    await createMovieList(movies);
-} 
+export const movieListAPI = async (
+  url = "/3/movie/popular?language=en-US&page=1"
+) => {
+  const api = await ApiFetch(url);
+  movies = api.results;
+  await createMovieList(movies);
+};
 
 const ApiFetch = async (url) => {
-    let json;
-    const options = {
-        method: 'GET',
-        headers: {
-            accept: 'application/json',
-            // Authorization: `Bearer ${ApiToken}`
-          }
-      };
+  let json;
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      // Authorization: `Bearer ${ApiToken}`
+    },
+  };
 
-      try {
-          const response = await fetch(`https://api.themoviedb.org${url}${ url.indexOf('?')!==-1?"&":"?" }api_key=${ApiKey}`, options)
-          json = await response.json();
-      } catch (error) {
-        console.error(error);
-      }
-    return json;
-}
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org${url}${
+        url.indexOf("?") !== -1 ? "&" : "?"
+      }api_key=${ApiKey}`,
+      options
+    );
+    json = await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+  return json;
+};
 
-const createMovieList = async (movies)  => {
-    $movieList.textContent = '';
-    const movieListHTML = movies.map((e) =>
+const createMovieList = async (movies) => {
+  $movieList.textContent = "";
+  const movieListHTML = movies
+    .map(
+      (e) =>
         `
         <div class="movie ${e.id}" onClick="alert('영화 id: ${e.id}')">
             <img src="https://image.tmdb.org/t/p/w500${e.poster_path}" alt="${e.id}">
@@ -52,6 +64,7 @@ const createMovieList = async (movies)  => {
             </div>
         </div>
         `
-    ).join("");
-    $movieList.insertAdjacentHTML('beforeend', movieListHTML);
-}
+    )
+    .join("");
+  $movieList.insertAdjacentHTML("beforeend", movieListHTML);
+};
