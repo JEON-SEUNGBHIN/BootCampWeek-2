91bcf85fb117b502e4d289838c388a30ab3e1b9b
Sol Park <solpark16@gmail.com>
2024-05-03 12:57:01 +0900

    test commit

--- js/detail.js
+++ js/detail.js
@@ -1,49 +1,55 @@
+
+import { ApiFetch } from "./movie.js"
+import { handleAddReviews, loadReviews } from "./review.js"
+import { currentLanguage } from "./language.js"
+
+const $reviewsForm = document.querySelector("#review-form");
+$reviewsForm.addEventListener('submit', handleAddReviews);
+
+
+(function init() {
+    loadReviews();
+})()
+
 // detail.js
 
 // API 키
-const ApiKey = "66f371611b5b7314fe42cbf067b62f1c";
+const ApiKey = '66f371611b5b7314fe42cbf067b62f1c';
 
 // 영화 상세 데이터를 가져오는 함수
 const fetchMovieDetails = async (movieId) => {
-  const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${ApiKey}&language=en-US&append_to_response=credits`;
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error("Failed to fetch movie details");
+    const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${ApiKey}&language=${currentLanguage}&append_to_response=credits`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Failed to fetch movie details');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching movie details:', error);
     }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching movie details:", error);
-  }
-};
+}
 
 // 영화 상세 데이터를 가져와서 화면에 표시하는 함수
 const displayMovieDetails = (movieDetails) => {
-  // detail_main 요소 선택
-  const detailMain = document.querySelector(".detail_main");
-  const directors = movieDetails.credits.crew.filter(
-    (member) => member.job === "Director"
-  );
-  const directorNames = directors.map((director) => director.name).join(", ");
+    // detail_main 요소 선택
+    const detailMain = document.querySelector('.detail_main');
+    const directors = movieDetails.credits.crew.filter(member => member.job === "Director");
+    const directorNames = directors.map(director => director.name).join(', ');
 
-  // 출연 배우 정보 추출
-  const actors = movieDetails.credits.cast.slice(0, 10);
-  const actorNames = actors.map((actor) => actor.name).join(", ");
+    // 출연 배우 정보 추출
+    const actors = movieDetails.credits.cast.slice(0, 10);
+    const actorNames = actors.map(actor => actor.name).join(', ');
 
-  // detail_main 요소의 innerHTML을 채워 넣음
-  detailMain.innerHTML = `
+    // detail_main 요소의 innerHTML을 채워 넣음
+    detailMain.innerHTML = `
             <div class="img_container">
-            <div class="detail_img"
-                style="background-image:
-                url('https://image.tmdb.org/t/p/w500${
-                  movieDetails.poster_path
-                }')">
-                <img src="https://image.tmdb.org/t/p/w500${
-                  movieDetails.poster_path
-                }" alt="${movieDetails.title}">
-              </div>
-            </div>
+              <img src="https://image.tmdb.org/t/p/w500${movieDetails.poster_path}" 
+                  alt="${movieDetails.title}" class="detail_img">
+                <img src="https://image.tmdb.org/t/p/w500${movieDetails.poster_path}" 
+                    alt="${movieDetails.title}" class="detail_main_img">
+                </div>
             <div class="content_container">
                 <div class="detail_box1">
                     <h2 class="detail_title">${movieDetails.title}</h2>
@@ -69,7 +75,7 @@ const displayMovieDetails = (movieDetails) => {
                       .map((genre) => genre.name)
                       .join(", ")}</h5>
                 </div>
-                <hr class="detail_box_hr">
+                <hr class="detail_box3_hr">
                 <div class="detail_box3">
                     <div class="detail_director">
                         <h4 class="director_title">감독</h4>
@@ -84,7 +90,7 @@ const displayMovieDetails = (movieDetails) => {
                         <h5 class="detail_plot">${movieDetails.overview}</h5>
                     </div>
                 </div>
-                <hr class="detail_box_hr">
+                <hr class="detail_box3_hr">
             </div>
         `;
 
