44fa9d2f8fade816888c1eb9967bf743acc060e1
Kim ByeongJun <test@test.com>
2024-05-07 20:51:10 +0900

    heart.html에서의 한영전환 버튼 이벤트 리스너 동작 버그 수정 완료

--- js/heart.js
+++ js/heart.js
@@ -4,7 +4,7 @@ let hearts = JSON.parse(localStorage.getItem("hearts"));
 function displayHeartedMovies(heartedList) {
   const pickMovie = document.querySelector(".pick_movie");
   const pickNothing = document.querySelector(".pick_nothing")
-  if (heartedList.length === 0){
+  if (heartedList.length === 0) {
     pickNothing.style.display = 'block'
   }
   heartedList.forEach((e) => {
@@ -26,7 +26,7 @@ function displayHeartedMovies(heartedList) {
 
 const getMovies = async (hearts) => {
   let heartedList = [];
-  if (hearts !== null){
+  if (hearts !== null) {
     for (let i = 0; i < hearts.length; i++) {
       heartedList.push(
         await ApiFetch(`/3/movie/${hearts[i]}?original_language="eu" `)
@@ -34,6 +34,23 @@ const getMovies = async (hearts) => {
     }
   }
   displayHeartedMovies(heartedList);
+
+  // 언어변경 기능
+  document.getElementById('lang_change_btn_heart').addEventListener('click', async () => {
+    (localStorage.getItem("currentLanguage")) === 'en-US' ? localStorage.setItem("currentLanguage", 'ko-KR') : localStorage.setItem("currentLanguage", 'en-US');
+    // 이전에 표시된 카드들을 모두 제거
+    const pickMovie = document.querySelector(".pick_movie");
+    pickMovie.innerHTML = '';
+    // 바뀐 언어 정보를 포함하여 API fetch 새로하기, ApiFetch 코드 재사용 안 함 (추후 과제)
+    const newMovies = await Promise.all(hearts.map(async (heartId) => {
+      const url = `https://api.themoviedb.org/3/movie/${heartId}?original_language=%22eu%22%20&language=${localStorage.getItem("currentLanguage")}&api_key=21ccf5793f9e51cfba0198fa23b3d541`;
+      const movieDetails = await fetch(url);
+      return movieDetails.json();
+    }));
+    displayHeartedMovies(newMovies);
+  });
+
+
 };
 (function init() {
   getMovies(hearts);
